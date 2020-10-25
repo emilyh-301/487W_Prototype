addb63b527381dc6d2f2d24548b9c23ba5a5e0e9
bendstein <bjg5493@psu.edu>
2020-10-25 19:23:07 -0400

    cart

---
+++ src/main/java/com/hotel/controller/CartController.java
@@ -0,0 +1,26 @@
+package com.hotel.controller;
+
+import com.hotel.model.item.Cart;
+import com.hotel.service.item.CartService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cart")
+public class CartController {
+
+    private final CartService cart_service;
+
+    public CartController(CartService cart_service) {
+        this.cart_service = cart_service;
+    }
+
+    @GetMapping("/")
+    public String viewCart(Model model) {
+        Cart cart = cart_service.find(0);
+        model.addAttribute("Cart", cart.getItems());
+        return "cart";
+    }
+}
