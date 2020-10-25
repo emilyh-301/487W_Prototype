0ef2cdf0d5c3368c1a69c2b7303f1e94c2d0bce8
bendstein <bjg5493@psu.edu>
2020-10-25 19:38:07 -0400

    cart

--- build/resources/main/schema.sql
+++ build/resources/main/schema.sql
@@ -1,25 +1,58 @@
 DROP TABLE IF EXISTS GROUP_MEMBER;
 DROP TABLE IF EXISTS Allergens;
+DROP TABLE IF EXISTS cartItems;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS cart;
 DROP TABLE IF EXISTS menuItems;
+DROP TABLE IF EXISTS itemAllergens;
 DROP TABLE IF EXISTS staffIDs;
 DROP TABLE IF EXISTS wakeUpCallRequests;
 DROP TABLE IF EXISTS maintenance_requests;
 DROP TABLE IF EXISTS general_requests;
 DROP TABLE IF EXISTS request;
 
+create table cart (
+    cart_id int primary key,
+    completed boolean not null,
+    room int not null
+);
+
+create table orders (
+    order_id int primary key,
+    cart_id int not null,
+    foreign key (cart_id) references cart,
+    status varchar(10) not null,
+    timeOfRequest DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
 create table menuItems (
     item_id int primary key,
-    name varchar(50),
+    name varchar(50) not null,
     price decimal(4,2) not null,
     description varchar(300) not null,
-    --allergens varchar(100),
-    image blob -- should be not null, add pics later
+    image varchar(100)
+);
+
+create table cartItems (
+    id int primary key,
+    item_id int,
+    foreign key (item_id) references menuItems,
+    quantity int DEFAULT 1,
+    notes varchar(100),
+    cart_id int not null,
+    foreign key (cart_id) references cart
 );
 
 create table Allergens (
-    allergen varchar(25) primary key ,
-    item_id int not null,
-    foreign key (item_id) references menuItems
+    allergen varchar(25) primary key
+);
+
+create table itemAllergens (
+    item_id int,
+    foreign key (item_id) references menuItems,
+    allergen varchar(25),
+    foreign key (allergen) references Allergens,
+    primary key (item_id, allergen)
 );
 
 create table staffIDs (
