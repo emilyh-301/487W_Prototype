37e47516e97cf3c52783bcb520f46bea2acfaf00
bendstein <bjg5493@psu.edu>
2020-10-25 20:08:46 -0400

    checkout

---
+++ src/main/java/com/hotel/controller/CheckoutController.java
@@ -0,0 +1,27 @@
+package com.hotel.controller;
+
+import com.hotel.model.item.Cart;
+import com.hotel.service.item.CartService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/checkout")
+public class CheckoutController {
+
+    private final CartService service;
+
+    public CheckoutController(CartService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/")
+    public String viewCheckout(Model model) {
+        Cart cart = service.find(0);
+
+        model.addAttribute("cart", cart);
+        return "checkout";
+    }
+}
