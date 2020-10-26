b431fe66801217fb8d5bbb0c46199b944c9939d3
bendstein <bjg5493@psu.edu>
2020-10-25 21:28:02 -0400

    commit

--- src/main/java/com/hotel/database/load/DatabaseLoader.java
+++ src/main/java/com/hotel/database/load/DatabaseLoader.java
@@ -1,13 +1,17 @@
 package com.hotel.database.load;
 
 import com.hotel.database.MemberDatabase;
+import com.hotel.database.item.CartDatabase;
 import com.hotel.database.item.MenuItemDatabase;
 import com.hotel.database.request.*;
 import com.hotel.database.staff.StaffDatabase;
+import com.hotel.model.item.Cart;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
 
 /**
  * Called when the application is initialized or refreshed.
@@ -31,22 +35,24 @@ public class DatabaseLoader implements ApplicationListener<ContextRefreshedEvent
     @Qualifier("Wakeup")
     private final WakeUpRequestDatabase wakeupRepo;
 
+    private final CartDatabase cartDatabase;
+
     private final MenuItemDatabase itemRepo;
 
     public DatabaseLoader(MemberDatabase database, StaffDatabase staffRepo, RequestDatabase requestRepo,
                           MaintenanceRequestDatabase maintenanceRepo, GeneralRequestDatabase generalRepo,
-                          WakeUpRequestDatabase wakeupRepo, MenuItemDatabase itemRepo) {
+                          WakeUpRequestDatabase wakeupRepo, CartDatabase cartDatabase, MenuItemDatabase itemRepo) {
         this.database = database;
         this.staffRepo = staffRepo;
         this.requestRepo = requestRepo;
         this.maintenanceRepo = maintenanceRepo;
         this.generalRepo = generalRepo;
         this.wakeupRepo = wakeupRepo;
+        this.cartDatabase = cartDatabase;
         this.itemRepo = itemRepo;
     }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
-
     }
 }
