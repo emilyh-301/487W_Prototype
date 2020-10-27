e0e5a3644d7dac5ed1fd7f6d8357db423da2b537
emilyh-301 <36495239+emilyh-301@users.noreply.github.com>
2020-10-26 22:35:02 -0400

    working on checkout

--- src/main/java/com/hotel/controller/CheckoutController.java
+++ src/main/java/com/hotel/controller/CheckoutController.java
@@ -1,7 +1,9 @@
 package com.hotel.controller;
 
 import com.hotel.model.item.Cart;
+import com.hotel.model.item.CartItem;
 import com.hotel.service.item.CartService;
+import com.hotel.service.user.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +14,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CheckoutController {
 
     private final CartService service;
+    private final UserService userService;
 
-    public CheckoutController(CartService service) {
+    public CheckoutController(CartService service, UserService userService) {
         this.service = service;
+        this.userService = userService;
     }
 
     @GetMapping("/")
     public String viewCheckout(Model model) {
-        Cart cart = service.find(0);
+        Cart cart = userService.getActiveCart(userService.getCurrentUser());
+        double sum = 0;
+        if(cart != null) {
+            for (CartItem ci : cart.getItems()) {
+                sum += ci.getQuantity() * ci.getItem().getPrice().doubleValue();
+            }
+        }
         model.addAttribute("cart", cart);
+        model.addAttribute("total", sum);
         return "checkout";
     }
 }
