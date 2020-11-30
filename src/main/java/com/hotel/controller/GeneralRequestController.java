134c29a01132754f51e7b3658424ba2bf0c6162f
emilyh-301 <36495239+emilyh-301@users.noreply.github.com>
2020-11-30 13:05:37 -0500

    Added a basic general request controller and started general request html

---
+++ src/main/java/com/hotel/controller/GeneralRequestController.java
@@ -0,0 +1,14 @@
+package com.hotel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/generalrequest")
+public class GeneralRequestController {
+
+    public GeneralRequestController() {
+    }
+
+
+}
